020f4707fb9a2cacf3f372f54725ad92bb91b837
Bernardo P Zapelini <94933673+bino1432@users.noreply.github.com>
2023-06-06 16:50:10 -0300

    Add files via upload

---
+++ LojaVirtual/src/LojaVirtual/Main.java
@@ -0,0 +1,13 @@
+package LojaVirtual;
+/**
+ * 
+ * @author Bernardo Zapelini
+ * 
+ * CLASSE MAIN PARA RODAR O CODIGO
+ * 
+ */
+public class Main {
+	public static void main (String []args) {
+	
+	}
+}
