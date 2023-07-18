39c4508867e84516124714aa000a60aa20c78fe7
Bernardo P Zapelini <bernardozplz@gmail.com>
2023-07-18 14:29:09 -0300

    LojaVirtual

--- LojaVirtual/src/LojaVirtual/FrameCadastro.java
+++ LojaVirtual/src/LojaVirtual/FrameCadastro.java
@@ -12,6 +12,8 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JProgressBar;
+import java.awt.Font;
+import java.awt.Color;
 
 public class FrameCadastro extends JFrame {
 
@@ -48,75 +50,85 @@ public class FrameCadastro extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(196, 194, 189));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lbNome = new JLabel("Nome:");
-		lbNome.setBounds(32, 70, 46, 14);
+		lbNome.setFont(new Font("Calibri", Font.PLAIN, 17));
+		lbNome.setBounds(10, 55, 60, 14);
 		contentPane.add(lbNome);
 		
 		JLabel lbCPF = new JLabel("CPF:");
-		lbCPF.setBounds(212, 70, 46, 14);
+		lbCPF.setFont(new Font("Calibri", Font.PLAIN, 17));
+		lbCPF.setBounds(10, 195, 46, 17);
 		contentPane.add(lbCPF);
 		
 		JLabel lbEmail = new JLabel("Email:");
-		lbEmail.setBounds(32, 112, 46, 14);
+		lbEmail.setFont(new Font("Calibri", Font.PLAIN, 17));
+		lbEmail.setBounds(10, 90, 46, 17);
 		contentPane.add(lbEmail);
 		
 		JLabel lbLogin = new JLabel("Login:");
-		lbLogin.setBounds(32, 154, 46, 14);
+		lbLogin.setFont(new Font("Calibri", Font.PLAIN, 17));
+		lbLogin.setBounds(10, 125, 46, 14);
 		contentPane.add(lbLogin);
 		
 		JLabel lbSenha = new JLabel("Senha:");
-		lbSenha.setBounds(212, 154, 46, 14);
+		lbSenha.setFont(new Font("Calibri", Font.PLAIN, 17));
+		lbSenha.setBounds(10, 160, 60, 17);
 		contentPane.add(lbSenha);
 		
 		JLabel lblDataDeNascimento = new JLabel("Data de Nascimento:");
-		lblDataDeNascimento.setBounds(188, 112, 135, 14);
+		lblDataDeNascimento.setFont(new Font("Calibri", Font.PLAIN, 17));
+		lblDataDeNascimento.setBounds(180, 55, 158, 17);
 		contentPane.add(lblDataDeNascimento);
 		
 		JLabel lblNumeroTelefone = new JLabel("Numero Telefone:");
-		lblNumeroTelefone.setBounds(20, 194, 127, 14);
+		lblNumeroTelefone.setFont(new Font("Calibri", Font.PLAIN, 17));
+		lblNumeroTelefone.setBounds(180, 90, 127, 20);
 		contentPane.add(lblNumeroTelefone);
 		
 		txNome = new JTextField();
-		txNome.setBounds(78, 67, 100, 20);
+		txNome.setBounds(70, 55, 100, 20);
 		contentPane.add(txNome);
 		txNome.setColumns(10);
 		
 		txCPF = new JTextField();
 		txCPF.setColumns(10);
-		txCPF.setBounds(258, 67, 100, 20);
+		txCPF.setBounds(70, 195, 100, 20);
 		contentPane.add(txCPF);
 		
 		txEmail = new JTextField();
 		txEmail.setColumns(10);
-		txEmail.setBounds(78, 109, 100, 20);
+		txEmail.setBounds(70, 90, 100, 20);
 		contentPane.add(txEmail);
 		
 		txData = new JTextField();
 		txData.setColumns(10);
-		txData.setBounds(313, 109, 100, 20);
+		txData.setBounds(335, 55, 100, 20);
 		contentPane.add(txData);
 		
 		txLogin = new JTextField();
 		txLogin.setColumns(10);
-		txLogin.setBounds(78, 151, 100, 20);
+		txLogin.setBounds(70, 125, 100, 20);
 		contentPane.add(txLogin);
 		
 		txSenha = new JTextField();
 		txSenha.setColumns(10);
-		txSenha.setBounds(258, 151, 100, 20);
+		txSenha.setBounds(70, 160, 100, 20);
 		contentPane.add(txSenha);
 		
 		txNumero = new JTextField();
 		txNumero.setColumns(10);
-		txNumero.setBounds(125, 191, 100, 20);
+		txNumero.setBounds(335, 90, 100, 20);
 		contentPane.add(txNumero);
 		
 		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.setBackground(new Color(215, 211, 208));
+		btnCadastrar.setFont(new Font("Calibri", Font.PLAIN, 13));
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -133,11 +145,12 @@ public class FrameCadastro extends JFrame {
 				JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso!");
 			}
 		});
-		btnCadastrar.setBounds(246, 190, 112, 23);
+		btnCadastrar.setBounds(250, 160, 112, 23);
 		contentPane.add(btnCadastrar);
 		
 		JLabel lblBemVindo = new JLabel("Bem Vindo!");
-		lblBemVindo.setBounds(173, 18, 125, 14);
+		lblBemVindo.setFont(new Font("Calibri", Font.PLAIN, 17));
+		lblBemVindo.setBounds(70, 20, 125, 17);
 		contentPane.add(lblBemVindo);
 	}
 }
--- LojaVirtual/src/LojaVirtual/MainFrame.java
+++ LojaVirtual/src/LojaVirtual/MainFrame.java
@@ -9,6 +9,8 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Font;
 
 public class MainFrame extends JFrame {
 
@@ -38,37 +40,44 @@ public class MainFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 194);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(196, 194, 189));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lbBemVindo = new JLabel("Bem Vindo!");
-		lbBemVindo.setBounds(188, 25, 140, 26);
+		lbBemVindo.setFont(new Font("Calibri", Font.PLAIN, 17));
+		lbBemVindo.setForeground(new Color(0, 0, 0));
+		lbBemVindo.setBounds(177, 11, 140, 26);
 		contentPane.add(lbBemVindo);
 		
 		JLabel lbVoceDeseja = new JLabel("Você deseja:");
-		lbVoceDeseja.setBounds(188, 54, 133, 14);
+		lbVoceDeseja.setFont(new Font("Calibri", Font.PLAIN, 17));
+		lbVoceDeseja.setBounds(177, 40, 130, 20);
 		contentPane.add(lbVoceDeseja);
 		
 		JButton btnCadastro = new JButton("Cadastrar");
+		btnCadastro.setBackground(new Color(215, 211, 208));
+		btnCadastro.setFont(new Font("Calibri", Font.PLAIN, 13));
 		btnCadastro.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				FrameCadastro frame = new FrameCadastro();
 				frame.setVisible(true);
 			}
 		});
-		btnCadastro.setBounds(73, 96, 112, 23);
+		btnCadastro.setBounds(167, 79, 112, 23);
 		contentPane.add(btnCadastro); 
 		
 		JButton btnLogin = new JButton("Logar");
+		btnLogin.setFont(new Font("Calibri", Font.PLAIN, 13));
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				FrameLogin frame = new FrameLogin();
 				frame.setVisible(true);
 			}
 		});
-		btnLogin.setBounds(245, 96, 112, 23);
+		btnLogin.setBounds(167, 113, 112, 23);
 		contentPane.add(btnLogin);
 	}
 }
